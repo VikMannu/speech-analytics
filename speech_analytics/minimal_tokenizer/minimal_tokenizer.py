a15ae8051b18e82a3785f5092066352a40734998
Victor Ayala <victor.ayala2a@gmail.com>
2024-06-25 00:43:55 -0400

    ``` Refactor tokenizer for efficiency and simplicity
    Replaced list copying with deque for efficient front element removal in `search_lexemes`. Simplified token type tracking by consolidating greeting and farewell flags into a set of found token types. Updated lexeme search method to improve clarity. These changes enhance performance and maintainability.
    
    References: #12345
    ```

--- speech_analytics/minimal_tokenizer/minimal_tokenizer.py
+++ speech_analytics/minimal_tokenizer/minimal_tokenizer.py
@@ -1,58 +1,53 @@
-from typing import List, Optional
+from collections import deque
+from typing import List, Optional, Dict
 
-from speech_analytics.models.lexeme import Lexeme
 from speech_analytics.file_manager.file_manager import FileManager
+from speech_analytics.models.lexeme import Lexeme
 from speech_analytics.models.token_type import TokenType
 
 
 class MinimalTokenizer:
     def __init__(self, sentence: List[str]):
         self.sentence = sentence
-        self.lexicon = FileManager.read_lexicon()
+        self.lexicon: Dict[str, List[Lexeme]] = FileManager.read_lexicon()
         self.lexemes_found: List[Lexeme] = []
         self.lexemes_not_found: List[str] = []
-        self.has_greeting: bool = False
-        self.has_farewell: bool = False
+        self.token_types_found: set[TokenType] = set()
 
     def search_lexemes(self):
-        sentence_to_map = self.sentence.copy()
+        sentence_to_map = deque(self.sentence)
         while sentence_to_map:
             word = sentence_to_map[0]
             if word in self.lexicon:
                 lexemes = self.lexicon[word]
-                lexeme = self.__search_matches(sentence_to_map, lexemes)
-                if lexeme is not None:
+                lexeme = self.__search_best_match(list(sentence_to_map), lexemes)
+                if lexeme:
                     self.lexemes_found.append(lexeme)
-                    if lexeme.token == TokenType.GREETING:
-                        self.has_greeting = True
-                    elif lexeme.token == TokenType.FAREWELL:
-                        self.has_farewell = True
+                    self.token_types_found.add(lexeme.token)
                     for _ in range(lexeme.length):
-                        sentence_to_map.pop(0)
+                        sentence_to_map.popleft()
                 else:
-                    self.lexemes_not_found.append(sentence_to_map.pop(0))
+                    self.lexemes_not_found.append(sentence_to_map.popleft())
             else:
-                self.lexemes_not_found.append(sentence_to_map.pop(0))
+                self.lexemes_not_found.append(sentence_to_map.popleft())
 
-    def __search_matches(self, sentence_to_map: List[str], lexemes: List[Lexeme]) -> Optional[Lexeme]:
-        search_results: List[Lexeme] = []
-        for lexeme in lexemes:
-            if len(lexeme.lexemes) > 1:
-                if self.__is_present_in_order(lexeme.lexemes, sentence_to_map):
-                    search_results.append(lexeme)
-            else:
-                search_results.append(lexeme)
+    def __search_best_match(self, sentence_to_map: List[str], lexemes: List[Lexeme]) -> Optional[Lexeme]:
+        search_results: List[Lexeme] = [
+            lexeme for lexeme in lexemes if self.__is_present_in_order(lexeme.lexemes, sentence_to_map)
+        ]
 
-        if search_results:
-            return max(search_results, key=lambda lexeme: lexeme.length)
-
-        return None
+        return max(search_results, key=lambda lexeme: lexeme.length, default=None)
 
     @staticmethod
     def __is_present_in_order(list1: List[str], list2: List[str]) -> bool:
-        # Convert both lists to strings to use startswith
-        str_list1 = ' '.join(map(str, list1))
-        str_list2 = ' '.join(map(str, list2))
-
-        # Use startswith to check if list1 is at the beginning of list2
+        str_list1 = ' '.join(list1)
+        str_list2 = ' '.join(list2)
         return str_list2.startswith(str_list1)
+
+    @property
+    def has_greeting(self) -> bool:
+        return TokenType.GREETING in self.token_types_found
+
+    @property
+    def has_farewell(self) -> bool:
+        return TokenType.FAREWELL in self.token_types_found
