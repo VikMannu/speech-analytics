b4a9803ae302c2db16ca2fc5e7a4f267c400f51e
Victor Ayala <victor.ayala2a@gmail.com>
2024-06-25 11:57:48 -0400

    ``` Add UI dialog for adding lexemes
    Introduced a new PyQt5-based UI dialog to add lexemes. The `AddLexeme` dialog allows users to input a word or phrase, select a token type from a dropdown, and choose a number between 1 and 5. This facilitates user interaction and data input within the application.
    
    References: #1234
    ```

---
+++ speech_analytics/ui/add_lexeme.py
@@ -0,0 +1,60 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QComboBox, QLineEdit, QPushButton, QSpinBox, QVBoxLayout, QLabel
+
+from speech_analytics.models.token_type import TokenType
+
+
+class AddLexeme(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        # Configuración básica del diálogo
+        self.setWindowTitle('Agregar lexema')
+        self.setGeometry(100, 100, 400, 200)
+
+        # Widgets del diálogo
+        self.label = QLabel('Ingrese palabra o frase:')
+        self.lineEdit = QLineEdit()
+
+        self.label2 = QLabel('Token:')
+        self.comboBox = QComboBox()
+        self.comboBox.addItems([token_type.title for token_type in TokenType])
+
+        self.label3 = QLabel('Seleccione un número del 1 al 5:')
+        self.spinBox = QSpinBox()
+        self.spinBox.setMinimum(1)
+        self.spinBox.setMaximum(5)
+
+        self.button = QPushButton('Aceptar')
+        self.button.clicked.connect(self.accept)
+
+        # Layout del diálogo
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.lineEdit)
+        layout.addWidget(self.label2)
+        layout.addWidget(self.comboBox)
+        layout.addWidget(self.label3)
+        layout.addWidget(self.spinBox)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def get_data(self):
+        # Obtén los datos ingresados por el usuario
+        text = self.lineEdit.text()
+        selected_title = self.comboBox.currentText()
+        selected_token_type = next(token_type for token_type in TokenType if token_type.title == selected_title)
+        number = self.spinBox.value()
+        return text, selected_token_type, number
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dialog = AddLexeme()
+    if dialog.exec_() == QDialog.Accepted:
+        data = dialog.get_data()
+        print("Texto:", data[0])
+        print("Opción:", data[1])
+        print("Número:", data[2])
+    sys.exit(app.exec_())
