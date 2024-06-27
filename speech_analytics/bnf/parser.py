e2bbbdc9166af95aac0e5bb0469d9fdde885a639
Victor Ayala <victor.ayala2a@gmail.com>
2024-06-27 16:54:07 -0400

    ### Subject: Remove redundant epsilon production from word_tail
    ### Body:
    Eliminated the epsilon (Ɛ) production from the `<word_tail>` BNF rule to simplify the grammar. This change eliminates unnecessary complexity and avoids potential parsing ambiguities, thereby improving the robustness and maintainability of the parser.
    
    Relevant to issue #42.

--- speech_analytics/bnf/parser.py
+++ speech_analytics/bnf/parser.py
@@ -11,7 +11,6 @@ BNF
 
 <word_tail> ::= <letter> <word_tail> { word_tail = letter + word_tail }
               | <punctuation> { '' }
-              | Ɛ { '' }
 
 <letter> ::= "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "ñ" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z"
             | "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "Ñ" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
